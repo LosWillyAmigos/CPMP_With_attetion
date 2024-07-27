5522e20b523c49789da24ba900dceb2d96fda559
Thomas <thomas.molina.s@mail.pucv.cl>
2024-07-27 18:34:51 -0400

    Refactor some functions and fix bug

--- attentional_cpmp/utils/hyperparameter_search/HyperparameterStudy.py
+++ attentional_cpmp/utils/hyperparameter_search/HyperparameterStudy.py
@@ -31,7 +31,8 @@ class HyperparameterStudy:
                 dir: str = None,
                 patience: int = 3,
                 verbose: int = 1,
-                restore_best_weights: bool = True) -> None:
+                restore_best_weights: bool = True,
+                metrics_monitor_callback: str = 'val_loss') -> None:
     # Exceptions
     if X_train is None or Y_train is None or X_val is None or Y_val is None:
        raise ValueError("There is a problem with data...")
@@ -71,30 +72,63 @@ class HyperparameterStudy:
     self.__patience = patience
     self.__verbose = verbose
     self.__restore_best_weights = restore_best_weights
+    self.__metrics_monitor_callback = metrics_monitor_callback
 
     self.inser_manual_trials()
   
-  def set_training_data(self, H: int, 
-                        optimizer: str = 'Adam', 
+  def set_model_config(self, H: int, 
+                        optimizer: str = 'Adam') -> None:
+    """
+    Set non-suggested parameters to create a model
+
+    Args:
+        H (int) : Input vector lenght.
+        optimizer (str) : Type of optimizer.
+    """
+    self.__H = H
+    self.__optimizer = optimizer
+    
+
+  def set_fit_config(self,
+                     epochs: int = 20, 
+                     batch_size: int = 32,
+                     verbose: int = 1,
+                     metrics_monitor_callback: str = 'val_loss'):
+    """
+    Set configuration to train the model on objective function
+
+    Args:
+        epochs (int) : Non-negative number > 1.
+        batch_size (int) : training batches on .fit
+        verbose (int) : Show training and callbacks messages
+        matrics_monitor_callback (str) : Metric to monitor.
+
+    """
+    self.__epochs = epochs
+    self.__batch_size = batch_size
+    self.__verbose = verbose
+    self.__metrics_monitor_callback = metrics_monitor_callback
+
+  def set_training_data(self, 
                         X_train: np.ndarray = None, 
                         Y_train: np.ndarray = None, 
                         X_val: np.ndarray = None, 
-                        Y_val: np.ndarray = None, 
-                        epochs: int = 20, 
-                        batch_size: int = 32) -> None:
-    self.__H = H
-    self.__optimizer = optimizer
+                        Y_val: np.ndarray = None):
+    """
+    Set data to train the model on objective function
+    """
     self.__X_train = X_train
     self.__Y_train = Y_train
     self.__X_val = X_val
     self.__Y_val = Y_val
-    self.__epochs = epochs
-    self.__batch_size = batch_size
 
   def inser_manual_trials(self):
     pass
 
   def importance(self):
+    """
+    Show the importance of parameters after the optimization process
+    """
     param_importances = get_param_importances(self.__study)
 
     print("********** Importance of hyperparameters: **********")
@@ -102,6 +136,12 @@ class HyperparameterStudy:
         print(f"  {param}: {importance:.8f}")
   
   def save(self, filename: str = None):
+    """
+    Save the best hyperparameters in a .hyp file
+
+    Args:
+        filename (str) : Path + file name
+    """
     if filename is None: filename = self.__study_name
     with open(filename + '.hyp', 'w') as custom_file:
       best_params = self.__study.best_params
@@ -109,6 +149,14 @@ class HyperparameterStudy:
           custom_file.write(f"{clave}: {valor}\n")
 
   def new_study(self, study_name: str = "New_Study", direction: str = None):
+     
+     """
+     Reset study
+
+     Args:
+        study_name (str) : New study name.
+        direction (str) : minimize or maximize.
+     """
      self.__study = create_study(study_name=study_name, 
                                  direction=direction, 
                                  pruner=self.__pruner)
@@ -132,9 +180,10 @@ class HyperparameterStudy:
                           list_neuron_feed=list_neuron_feed,
                           list_neuron_hide=list_neuron_hide)
 
-      pruning_callback = TFKerasPruningCallback(trial, 'val_loss')
+      pruning_callback = TFKerasPruningCallback(trial, self.__metrics_monitor_callback)
       
       '''
+      Dont' uses
       early_stopping_callback = EarlyStopping(
           monitor='val_loss',  
           patience=self.__patience,         
@@ -144,7 +193,8 @@ class HyperparameterStudy:
       )
       '''
       best_hyp_saver = BestHyperparameterSaver(trial, 
-                                               monitor='val_loss', filename=self.__dir + "best_hyperparameter.json")
+                                               monitor=self.__metrics_monitor_callback, 
+                                               filename=self.__dir + "best_hyperparameter.json")
       
       history = model.fit(x=self.__X_train, y=self.__Y_train, 
                           epochs=self.__epochs, 
@@ -152,7 +202,6 @@ class HyperparameterStudy:
                           verbose=self.__verbose, 
                           validation_data=(self.__X_val, self.__Y_val),
                           callbacks=[pruning_callback, 
-                                     early_stopping_callback,
                                      best_hyp_saver])
       
       
@@ -161,4 +210,7 @@ class HyperparameterStudy:
       return val_loss
   
   def optimize(self):
+    """
+    Start the optimization process
+    """
     self.__study.optimize(func=self.objective, n_trials=self.__n_iterations)
