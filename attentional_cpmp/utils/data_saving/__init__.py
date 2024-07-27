b751eb861291901fc425d011afdfb1fca207860c
Thomas <thomas.molina.s@mail.pucv.cl>
2024-07-27 16:19:05 -0400

    Add function to module from data_json.py

---
+++ attentional_cpmp/utils/data_saving/__init__.py
@@ -0,0 +1,3 @@
+from attentional_cpmp.utils.data_saving.local import save_data, load_data
+from attentional_cpmp.utils.data_saving.mongodb import connect_to_server, save_data_mongo, load_data_mongo
+from attentional_cpmp.utils.data_saving.data_json import save_data_json, load_data_json
