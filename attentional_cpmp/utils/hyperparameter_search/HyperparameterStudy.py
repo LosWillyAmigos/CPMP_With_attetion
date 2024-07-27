eb494272d5fc53d33553ee0bf13a9fc13337ad6a
Thomas <thomas.molina.s@mail.pucv.cl>
2024-07-27 16:09:39 -0400

    Update of objective function with early stopping

--- attentional_cpmp/utils/hyperparameter_search/HyperparameterStudy.py
+++ attentional_cpmp/utils/hyperparameter_search/HyperparameterStudy.py
@@ -1,12 +1,16 @@
 from attentional_cpmp.model import create_model
+from attentional_cpmp.utils.callbacks import  BestHyperparameterSaver
+from attentional_cpmp.utils import create_directory
 
-from optuna.visualization import plot_param_importances
+from optuna.integration import TFKerasPruningCallback
 from optuna.importance import get_param_importances
 from optuna.pruners import HyperbandPruner
 from optuna import create_study
 from keras.backend import clear_session
+from keras.callbacks import EarlyStopping
 
 import numpy as np
+import os
 
 class HyperparameterStudy:
   def __init__(self, 
@@ -23,17 +27,37 @@ class HyperparameterStudy:
                 min_resource: int = 1, 
                 max_resource: int = 100,
                 reduction_factor: int = 3,
-                number_of_iterations: int = 10) -> None:
+                number_of_iterations: int = 10,
+                dir: str = None,
+                patience: int = 3,
+                verbose: int = 1,
+                restore_best_weights: bool = True) -> None:
+    # Exceptions
+    if X_train is None or Y_train is None or X_val is None or Y_val is None:
+       raise ValueError("There is a problem with data...")
+    
+    # Create a directory if don't exist or if 'dir' is None
+    if dir is None:
+       create_directory("hyperparameter_test/")
+       self.__dir = "hyperparameter_test/"
+    else: self.__dir = dir
+
+    if not os.path.exists(self.__dir + "best_hyperparameter.json"):
+      with open(self.__dir + "best_hyperparameter.json", 'w', encoding='utf-8') as file:
+        pass
+
+    # Param optimization
     self.__pruner = HyperbandPruner(min_resource=min_resource,
                                     max_resource=max_resource, 
                                     reduction_factor=reduction_factor)
-    self.__study = create_study(study_name=study_name, 
-                                direction=direction, 
-                                pruner=self.__pruner)
-    self.__n_iterations = number_of_iterations
-
     self.__study_name = study_name
-
+    self.__direction = direction
+    self.__study = create_study(study_name=self.__direction, 
+                                direction=self.__direction, 
+                                pruner=self.__pruner)
+    
+    # Param model
+    self.__n_iterations = number_of_iterations
     self.__H = H
     self.__optimizer = optimizer
     self.__X_train = X_train
@@ -43,38 +67,12 @@ class HyperparameterStudy:
     self.__epochs = epochs
     self.__batch_size = batch_size
     
+    # Callback config
+    self.__patience = patience
+    self.__verbose = verbose
+    self.__restore_best_weights = restore_best_weights
+
     self.inser_manual_trials()
-
-  def objective(self, trial):
-      clear_session()
-
-      # Hiperparametros variables
-      num_stacks = trial.suggest_int('num_stacks', 1, 15)
-      heads = trial.suggest_int('heads', 1, 15)
-      epsilon = trial.suggest_float('epsilon', 1e-8, 1e-4, log=True)
-      num_neurons_layers_feed = trial.suggest_int('num_neurons_layers_feed', 1, 50)
-      num_neurons_layers_hide = trial.suggest_int('num_neurons_layers_hide', 1, 50)
-      list_neuron_feed = [trial.suggest_int(f'list_neuron_feed_{i}', 1, 100) for i in range(num_neurons_layers_feed)]
-      list_neuron_hide = [trial.suggest_int(f'list_neuron_hide_{i}', 1, 100) for i in range(num_neurons_layers_hide)]
-
-      model = create_model(heads=heads,
-                          H=self.__H,
-                          optimizer=self.__optimizer,
-                          epsilon=epsilon,
-                          num_stacks=num_stacks,
-                          list_neuron_feed=list_neuron_feed,
-                          list_neuron_hide=list_neuron_hide)
-
-      history = model.fit(self.__X_train, self.__Y_train, 
-                          epochs=self.__epochs, 
-                          batch_size=self.__batch_size, 
-                          verbose=0, 
-                          validation_data=(self.__X_val, self.__Y_val))
-      
-      clear_session()
-      val_mse = history.history['mse'][-1]
-
-      return val_mse
   
   def set_training_data(self, H: int, 
                         optimizer: str = 'Adam', 
@@ -96,18 +94,12 @@ class HyperparameterStudy:
   def inser_manual_trials(self):
     pass
 
-  def optimize(self):
-    self.__study.optimize(func=self.objective, n_trials=self.__n_iterations)
-
   def importance(self):
-    param_importances = get_param_importances(study)
+    param_importances = get_param_importances(self.__study)
 
     print("********** Importance of hyperparameters: **********")
     for param, importance in param_importances.items():
         print(f"  {param}: {importance:.8f}")
-  
-  def display(self):
-    plot_param_importances(self.__study)
   
   def save(self, filename: str = None):
     if filename is None: filename = self.__study_name
@@ -115,3 +107,58 @@ class HyperparameterStudy:
       best_params = self.__study.best_params
       for clave, valor in best_params.items():
           custom_file.write(f"{clave}: {valor}\n")
+
+  def new_study(self, study_name: str = "New_Study", direction: str = None):
+     self.__study = create_study(study_name=study_name, 
+                                 direction=direction, 
+                                 pruner=self.__pruner)
+
+  def objective(self, trial):
+      clear_session()
+      # Hiperparametros variables
+      num_stacks = trial.suggest_int('num_stacks', 1, 15)
+      heads = trial.suggest_int('heads', 1, 15)
+      epsilon = trial.suggest_float('epsilon', 1e-8, 1e-4, log=True)
+      num_neurons_layers_feed = trial.suggest_int('num_neurons_layers_feed', 1, 50)
+      num_neurons_layers_hide = trial.suggest_int('num_neurons_layers_hide', 1, 50)
+      list_neuron_feed = [trial.suggest_int(f'list_neuron_feed_{i}', 1, 80) for i in range(num_neurons_layers_feed)]
+      list_neuron_hide = [trial.suggest_int(f'list_neuron_hide_{i}', 1, 80) for i in range(num_neurons_layers_hide)]
+
+      model = create_model(heads=heads,
+                          H=self.__H,
+                          optimizer=self.__optimizer,
+                          epsilon=epsilon,
+                          num_stacks=num_stacks,
+                          list_neuron_feed=list_neuron_feed,
+                          list_neuron_hide=list_neuron_hide)
+
+      pruning_callback = TFKerasPruningCallback(trial, 'val_loss')
+      
+      '''
+      early_stopping_callback = EarlyStopping(
+          monitor='val_loss',  
+          patience=self.__patience,         
+          mode='min',          
+          verbose=self.__verbose,          
+          restore_best_weights=self.__restore_best_weights
+      )
+      '''
+      best_hyp_saver = BestHyperparameterSaver(trial, 
+                                               monitor='val_loss', filename=self.__dir + "best_hyperparameter.json")
+      
+      history = model.fit(x=self.__X_train, y=self.__Y_train, 
+                          epochs=self.__epochs, 
+                          batch_size=self.__batch_size, 
+                          verbose=self.__verbose, 
+                          validation_data=(self.__X_val, self.__Y_val),
+                          callbacks=[pruning_callback, 
+                                     early_stopping_callback,
+                                     best_hyp_saver])
+      
+      
+      val_loss = history.history['val_loss'][-1]
+
+      return val_loss
+  
+  def optimize(self):
+    self.__study.optimize(func=self.objective, n_trials=self.__n_iterations)
