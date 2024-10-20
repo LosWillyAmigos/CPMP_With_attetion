8916845535a7042afbbdef3d6c324ffad8980d6f
Thomas Molina <101366318+EDANHS@users.noreply.github.com>
2024-10-20 14:11:15 -0300

    Update layers
    Refactor layers:
    1. FeedForward.
    2. ModelCPMP.
    3. StackAttention.

--- attentional_cpmp/layers/FeedForward.py
+++ attentional_cpmp/layers/FeedForward.py
@@ -1,11 +1,7 @@
-from keras.regularizers import L1L2, L1, L2
-
 from keras.layers import Layer
 from keras.layers import Dense
 from keras.layers import Dropout
-from keras import Sequential
-from typing import Any
-
+from keras.models import Sequential
 import tensorflow as tf
 
 class FeedForward(Layer):
@@ -15,117 +11,57 @@ class FeedForward(Layer):
     This class implements a simple feedforward neural network layer using TensorFlow's Keras API. The network consists of multiple dense layers with dropout regularization.
 
     Attributes:
-        
-    Methods:
-        __init__()
+        dim (int): The input dimension of the network. Default value is 5 for five column models.
+        activation (str): The activation function applied to each dense layer. Default is 'sigmoid'.
+        dim_output (int): The output dimension of the network. Default is 5.
 
-        call(self, inputs, training=True, **kwargs)
+    Methods:
+        __init__(self, dim=5, activation='sigmoid', dim_output=5)
+            Initializes the FeedForward layer with specified input dimension, activation function, and output dimension.
+
+        call(self, inputs)
             Defines the forward pass of the network.
 
     Usage:
-    
+        # Create a FeedForward layer
+        feedforward_layer = FeedForward(dim=10, activation='relu', dim_output=3)
+
+        # Perform a forward pass
+        output = feedforward_layer(inputs)
     """
     def __init__(self, 
-                 dim_input: int = None, 
-                 dim_output: int = None, 
-                 activation: str = 'linear',
+                 dim_input: int,  
+                 dim_output: int,
                  list_neurons: list[int] = None,
-                 use_bias: bool=True,
-                 kernel_initializer_feed: str = "glorot_uniform",
-                 bias_initializer_feed: str="zeros",
-                 kernel_regularizer_feed: Any | None = "L1L2",
-                 kernel_regularizer_feed_value: float = 0.01,
-                 bias_regularizer_feed: Any | None = "L1L2",
-                 bias_regularizer_feed_value: float = 0.01,
-                 activity_regularizer_feed: Any | None = None,
-                 kernel_constraint_feed: Any | None = None,
-                 bias_constraint_feed: Any | None = None,
-                 rate: float = 0.000001,
-                 noise_shape: Any | None = None,
-                 seed: Any | None = None,
+                 activation: str = 'sigmoid',
+                 rate: float = 0.00001,
                  n_dropout: int = 1,
                  **kwargs) -> None:
-        super(FeedForward, self).__init__()
-        if dim_input is None or dim_output is None:
-            raise ValueError("Input or Output is None")
-        
-        ## Kernel regilarizer to overfitting
-        if kernel_regularizer_feed is not None:
-            if kernel_regularizer_feed== "L1":
-                k_r_f = L1(l1=kernel_regularizer_feed_value)
-            if kernel_regularizer_feed == "L2":
-                k_r_f = L2(l2=kernel_regularizer_feed_value)
-            if kernel_regularizer_feed == "L1L2":
-                k_r_f = L1L2(l1=kernel_regularizer_feed_value, l2=kernel_regularizer_feed_value)
-        else:
-            k_r_f = None
-
-        ## Bias regilarizer to overfitting
-        if bias_regularizer_feed is not None:
-            if bias_regularizer_feed == "L1":
-                b_r_f = L1(l1=bias_regularizer_feed_value)
-            if bias_regularizer_feed == "L2":
-                b_r_f = L2(l2=bias_regularizer_feed_value)
-            if bias_regularizer_feed == "L1L2":
-                b_r_f = L1L2(l1=bias_regularizer_feed_value, l2=bias_regularizer_feed_value)
-        else:
-            b_r_f = None
+        # Verificar si los parámetros requeridos tienen valores
+        if dim_input is None:
+            raise ValueError("dim_input has no value.")
+        super(FeedForward,self).__init__()
         self.__feed = Sequential()
         
         self.__feed.add(Dense(units=dim_input, 
                               activation=activation, 
-                              input_shape=((None, dim_input)),
-                              use_bias=use_bias,
-                              kernel_initializer=kernel_initializer_feed,
-                              bias_initializer=bias_initializer_feed,
-                              kernel_regularizer=k_r_f,
-                              bias_regularizer=b_r_f,
-                              activity_regularizer=activity_regularizer_feed,
-                              kernel_constraint=kernel_constraint_feed,
-                              bias_constraint=bias_constraint_feed))
+                              input_shape=((None, dim_input))))
         
         if list_neurons is not None:
             for i, num_neuron in enumerate(list_neurons[0:], start=0):
                 if i % 2 == 0:
                     self.__feed.add(Dense(units=num_neuron, 
-                                      activation=activation,
-                                      use_bias=use_bias,
-                                      kernel_initializer=kernel_initializer_feed,
-                                      bias_initializer=bias_initializer_feed,
-                                      kernel_regularizer=k_r_f,
-                                      bias_regularizer=b_r_f,
-                                      activity_regularizer=activity_regularizer_feed,
-                                      kernel_constraint=kernel_constraint_feed,
-                                      bias_constraint=bias_constraint_feed))
+                                      activation=activation))
                 else:
                     self.__feed.add(Dense(units=num_neuron, 
-                                      activation="linear",
-                                      use_bias=use_bias,
-                                      kernel_initializer=kernel_initializer_feed,
-                                      bias_initializer=bias_initializer_feed,
-                                      kernel_regularizer=k_r_f,
-                                      bias_regularizer=b_r_f,
-                                      activity_regularizer=activity_regularizer_feed,
-                                      kernel_constraint=kernel_constraint_feed,
-                                      bias_constraint=bias_constraint_feed))
-
-                if ((i+1) % n_dropout == 0) and rate > 0:
-                    self.__feed.add(Dropout(rate=rate, 
-                                            noise_shape=noise_shape, 
-                                            seed=seed))
+                                      activation="linear"))
+                if n_dropout > 0:
+                    if ((i+1) % n_dropout == 0) and rate > 0:
+                        self.__feed.add(Dropout(rate=rate))
         
         self.__feed.add(Dense(units=dim_output, 
-                              activation=activation,
-                              use_bias=use_bias,
-                              kernel_initializer=kernel_initializer_feed,
-                              bias_initializer=bias_initializer_feed,
-                              kernel_regularizer=k_r_f,
-                              bias_regularizer=b_r_f,
-                              activity_regularizer=activity_regularizer_feed,
-                              kernel_constraint=kernel_constraint_feed,
-                              bias_constraint=bias_constraint_feed))
-    
-    
-    @tf.function
-    def call(self, inputs: tf.TensorArray, training=True, **kwargs):
-        return self.__feed(inputs, training=training, **kwargs)
+                              activation=activation))
+
+    def call(self, inputs: tf.TensorArray, training=True , **kwargs):
+        out = self.__feed(inputs, training=training, **kwargs)
+        return out
--- attentional_cpmp/layers/ModelCPMP.py
+++ attentional_cpmp/layers/ModelCPMP.py
@@ -1,15 +1,11 @@
-from keras.regularizers import L1L2, L1, L2
-
-from keras.layers import Layer
+from keras.layers import Layer, Input
 from keras.layers import Flatten
-from keras.layers import Dense
-from keras.layers import Input
 from keras.models import Model
-from typing import Any
-
 from attentional_cpmp.layers import FeedForward
 from attentional_cpmp.layers import StackAttention
 import tensorflow as tf
+
+from typing import Any
 
 class ModelCPMP(Layer):
     """
@@ -22,12 +18,9 @@ class ModelCPMP(Layer):
         H (int): The dimension parameter used in stack attention and feedforward layers.
         activation (str): Activation function applied to the inner layers. Default is 'sigmoid'.
         epsilon (float): Small constant for numerical stability in layer normalization. Default is 1e-6.
-        list_neurons_feed (list[int]): Number of neurons for dense layers final.
-        list_neuron_hide (list[int]): Number of neurons for dense layers in stack attention.
-        n_dropout (int): It indicates that every few dense layers there will be a drop.
 
     Methods:
-        __init__(self, heads: int, H: int, activation='sigmoid', epsilon=1e-6,list_neurons_feed=None, list_neuron_hide=None)
+        __init__(self, heads: int, H: int, activation='sigmoid', epsilon=1e-6)
             Initializes the Model_CPMP layer with specified parameters.
 
         call(self, input_0, training=True)
@@ -40,196 +33,72 @@ class ModelCPMP(Layer):
         # Perform a forward pass
         output = model_cpmp_layer(input_0, training=True)
     """
-    def __init__(self, H: int = None,
-                  num_stacks: int = None,
-                  num_heads: int = None,
-                  activation_dense: Any | None = "linear",
-                  use_bias_dense: bool = True,
-                  kernel_initializer_dense: str = "glorot_uniform",
-                  bias_initializer_dense: str = "zeros",
-                  kernel_regularizer_dense: Any | None = "L1L2",
-                  kernel_regularizer_dense_value: float = 0.01,
-                  bias_regularizer_dense: Any | None = "L1L2",
-                  bias_regularizer_dense_value: float = 0.01,
-                  activity_regularizer_dense: Any | None = None,
-                  kernel_constraint_dense: Any | None = None,
-                  bias_constraint_dense: Any | None = None,
-                  dropout : float = 0.2,
-                  key_dim: int = None,
-                  value_dim: int = None,
-                  use_bias_multihead : bool = True,
-                  output_shape: int = 128,
-                  attention_axes: list[int] = None,
-                  kernel_initializer_multihead: str = "glorot_uniform",
-                  bias_initializer_multihead: str = "zeros",
-                  kernel_regularizer_multihead: str = "L1L2",
-                  kernel_regularizer_multihead_value: float = 0.01,
-                  bias_regularizer_multihead: str = "L1L2",
-                  bias_regularizer_multihead_value: float = 0.01,
-                  activity_regularizer_multihead: Any | None = None,
-                  kernel_constraint_multihead: Any | None = None,
-                  bias_constraint_multihead: Any | None = None,  
-                  dim_input_hide: int =  None,
-                  dim_output_hide: int = None,
-                  list_neurons_hide:list[int] = None,
-                  activation_feed_hide: str = 'linear',
-                  use_bias_feed_hide: bool=True,
-                  kernel_initializer_feed_hide: str = "glorot_uniform",
-                  bias_initializer_feed_hide: str ="zeros",
-                  kernel_regularizer_feed_hide: Any | None = "L1L2",
-                  kernel_regularizer_feed_value_hide: float = 0.01,
-                  bias_regularizer_feed_hide: Any | None = "L1L2",
-                  bias_regularizer_feed_value_hide: float = 0.01,
-                  activity_regularizer_feed_hide: Any | None = None,
-                  kernel_constraint_feed_hide: Any | None = None,
-                  bias_constraint_feed_hide: Any | None = None,
-                  rate_hide: float = 0.2,
-                  noise_shape_hide: Any | None = None,
-                  seed_hide: Any | None = None,
-                  n_dropout_hide: int = 1,
-                  axis: int = -1,
-                  epsilon:float = 1e-6,
-                  center: bool = True,
-                  scale: bool = True,
-                  beta_initializer: str = "zeros",
-                  gamma_initializer: str = "ones",
-                  beta_regularizer: Any | None = None,
-                  gamma_regularizer: Any | None = None,
-                  beta_constraint: Any | None = None,
-                  gamma_constraint: Any | None = None,
-                  activation_output: Any | None = "sigmoid",
-                  list_neurons_feed_output: list[int] = None,
-                  use_bias_output: bool = True,
-                  kernel_initializer_output: str = "glorot_uniform",
-                  bias_initializer_output: str = "zeros",
-                  kernel_regularizer_feed_output: Any | None = "L1L2",
-                  kernel_regularizer_feed_value_output: float = 0.01,
-                  bias_regularizer_feed_output: Any | None = "L1L2",
-                  bias_regularizer_feed_value_output: float = 0.01,
-                  activity_regularizer_feed_output: Any | None = None,
-                  kernel_constraint_feed_output: Any | None = None,
-                  bias_constraint_feed_output: Any | None = None,
-                  rate_output: float = 0.2,
-                  noise_shape_output: Any | None = None,
-                  seed_output: Any | None = None,
-                  n_dropout_output: int = 1,
-                  **kwargs) -> None:
+    def __init__(self, 
+                 dim: int,
+                 list_neurons_hide: list[int],
+                 list_neurons_feed: list[int],
+                 key_dim: Any,
+                 value_dim: Any | None = None,
+                 epsilon: float = 1e-6,
+                 dropout: float = 0,
+                 rate: float = 0.5,
+                 num_heads: int = 3, 
+                 num_stacks: int = None,
+                 activation_hide: str = 'sigmoid', 
+                 activation_feed: str = 'sigmoid',
+                 n_dropout_hide: int = 1,
+                 n_dropout_feed: int = 1,
+                 **kwargs) -> None:
         super(ModelCPMP, self).__init__()
-
-        if num_stacks is None or H is None:
+        if num_stacks is None or dim is None:
             raise ValueError("Arguments has no value.")
-        if dim_input_hide is None or dim_output_hide is None:
-            raise ValueError("dim_input_hide or dim_output_hide is None")
-        if key_dim is None:
-            raise ValueError("key_dim is None")
-        if output_shape is None:
-            raise ValueError("output_shape is None")
-
-        # Build model
-        inp = Input(shape=(None, H+1))
+        self.__heads = num_heads
+        self.__dim = dim + 1
+        self.__num_stack_attention = num_stacks
+        self.__epsilon = epsilon
         
-        ## Kernel regilarizer to overfitting
-        if kernel_regularizer_dense is not None:
-            if kernel_regularizer_dense == "L1":
-                k_r_d = L1(l1=kernel_regularizer_dense_value)
-            if kernel_regularizer_dense == "L2":
-                k_r_d = L2(l2=kernel_regularizer_dense_value)
-            if kernel_regularizer_dense == "L1L2":
-                k_r_d = L1L2(l1=kernel_regularizer_dense_value, l2=kernel_regularizer_dense_value)
-        else:
-            k_r_d = None
+        self.__list_neurons_hide = list_neurons_hide
+        self.__list_neurons_feed = list_neurons_feed
 
-        ## Bias regilarizer to overfitting
-        if bias_regularizer_dense is not None:
-            if bias_regularizer_dense == "L1":
-                b_r_d = L1(l1=bias_regularizer_dense_value)
-            if bias_regularizer_dense == "L2":
-                b_r_d = L2(l2=bias_regularizer_dense_value)
-            if bias_regularizer_dense == "L1L2":
-                b_r_d = L1L2(l1=bias_regularizer_dense_value, l2=bias_regularizer_dense_value)
-        else:
-            b_r_d = None
+        self.__key_dim = key_dim
+        self.__value_dim = value_dim
+        self.__dropout = dropout
+        self.__rate = rate
 
-        attention = Dense(units=output_shape,
-                          activation=activation_dense,
-                          use_bias=use_bias_dense,
-                          kernel_initializer=kernel_initializer_dense,
-                          bias_initializer=bias_initializer_dense,
-                          kernel_regularizer=k_r_d,
-                          bias_regularizer=b_r_d,
-                          activity_regularizer=activity_regularizer_dense,
-                          kernel_constraint=kernel_constraint_dense,
-                          bias_constraint=bias_constraint_dense)(inp)
+        self.__n_dropout_hide = n_dropout_hide
+        self.__n_dropout_feed = n_dropout_feed
+        self.__activation_hide = activation_hide
+        self.__activation_feed = activation_feed
 
-        for _ in range(num_stacks):
-            attention = StackAttention(num_heads=num_heads,
-                                       dropout=dropout,
-                                       key_dim=key_dim,
-                                       value_dim=value_dim,
-                                       use_bias_multihead=use_bias_multihead,
-                                       output_shape=output_shape,
-                                       attention_axes=attention_axes,
-                                       kernel_initializer_multihead=kernel_initializer_multihead,
-                                       bias_initializer_multihead=bias_initializer_multihead,
-                                       kernel_regularizer_multihead=kernel_regularizer_multihead,
-                                       kernel_regularizer_multihead_value=kernel_regularizer_multihead_value,
-                                       bias_regularizer_multihead=bias_regularizer_multihead,
-                                       bias_regularizer_multihead_value=bias_regularizer_multihead_value,
-                                       activity_regularizer_multihead=activity_regularizer_multihead,
-                                       kernel_constraint_multihead=kernel_constraint_multihead,
-                                       bias_constraint_multihead=bias_constraint_multihead,
-                                       dim_input=dim_input_hide,
-                                       dim_output=dim_output_hide,
-                                       list_neurons=list_neurons_hide,
-                                       activation_feed=activation_feed_hide,
-                                       use_bias_feed=use_bias_feed_hide,
-                                       kernel_initializer_feed=kernel_initializer_feed_hide,
-                                       bias_initializer_feed=bias_initializer_feed_hide,
-                                       kernel_regularizer_feed=kernel_regularizer_feed_hide,
-                                       kernel_regularizer_feed_value=kernel_regularizer_feed_value_hide,
-                                       bias_regularizer_feed=bias_regularizer_feed_hide,
-                                       bias_regularizer_feed_value=bias_regularizer_feed_value_hide,
-                                       activity_regularizer_feed=activity_regularizer_feed_hide,
-                                       kernel_constraint_feed=kernel_constraint_feed_hide,
-                                       bias_constraint_feed=bias_constraint_feed_hide,
-                                       rate=rate_hide,
-                                       noise_shape=noise_shape_hide,
-                                       seed=seed_hide,
-                                       n_dropout=n_dropout_hide,
-                                       axis=axis,
-                                       epsilon=epsilon,
-                                       center=center,
-                                       scale=scale,
-                                       beta_initializer=beta_initializer,
-                                       gamma_initializer=gamma_initializer,
-                                       beta_regularizer=beta_regularizer,
-                                       gamma_regularizer=gamma_regularizer,
-                                       beta_constraint=beta_constraint,
-                                       gamma_constraint=gamma_constraint)(attention, attention)
+        inp = Input(shape=(None, dim + 1))
+        attention = inp
+        for _ in range(self.__num_stack_attention):
+            attention = StackAttention(num_heads=self.__heads, 
+                                       dim_input=self.__dim,
+                                       dim_output=self.__dim,
+                                       list_neurons=self.__list_neurons_hide,
+                                       key_dim=self.__key_dim,
+                                       value_dim=self.__value_dim,
+                                       epsilon=self.__epsilon,
+                                       activation_feed_hide=self.__activation_hide,
+                                       n_dropout=self.__n_dropout_hide,
+                                       dropout=self.__dropout,
+                                       rate=self.__rate,
+                                       **kwargs)(attention, attention)
 
-        feed = FeedForward(dim_input=output_shape, 
+        feed = FeedForward(dim_input=self.__dim,
                            dim_output=1,
-                           activation_feed=activation_output, 
-                           list_neurons=list_neurons_feed_output,
-                           use_bias=use_bias_output,
-                           kernel_initializer_feed=kernel_initializer_output,
-                           bias_initializer_feed=bias_initializer_output,
-                           kernel_regularizer_feed= kernel_regularizer_feed_output,
-                           kernel_regularizer_feed_value=kernel_regularizer_feed_value_output,
-                           bias_regularizer_feed=bias_regularizer_feed_output,
-                           bias_regularizer_feed_value=bias_regularizer_feed_value_output,
-                           activity_regularizer_feed=activity_regularizer_feed_output,
-                           kernel_constraint_feed=kernel_constraint_feed_output,
-                           bias_constraint_feed=bias_constraint_feed_output,
-                           rate=rate_output,
-                           noise_shape=noise_shape_output,
-                           seed=seed_output,
-                           n_dropout=n_dropout_output)(attention)
+                           list_neurons=self.__list_neurons_feed,
+                           activation=self.__activation_feed,
+                           rate=self.__rate,
+                           n_dropout=self.__n_dropout_feed,
+                           **kwargs)(attention)
         flttn = Flatten()(feed)
-        
+
         self.__model_stacks = Model(inputs=inp, outputs=flttn)
-        
+
     
-    @tf.function 
-    def call(self, input_0: tf.TensorArray, training = True, **kwargs):
-        return self.__model_stacks(input_0, training=training, **kwargs)
+    @tf.function
+    def call(self, input_0: tf.TensorArray, training=True) -> None:
+        out = self.__model_stacks(input_0, training=training)
+        return out
