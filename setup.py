4827a020366f0e84dd63086900a43fb3a22d4120
EDANHS <thomas.molina@pucv.cl>
2024-12-29 20:24:43 -0300

    Add new req module - PANDAS

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "optuna-integration==3.6.0",
         "keras-tuner==1.4.7",
         "cpmp_ml @ git+https://github.com/rilianx/CPMP-ML.git@develop#egg=cpmp_ml"
+        "pandas==2.2.3"
     ],
     python_requires=">=3.10,<3.12",
     packages=find_packages(
