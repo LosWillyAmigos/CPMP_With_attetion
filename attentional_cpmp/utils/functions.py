9eba336d39a3795531307e107e1cbe181534da72
EDANHS <thomas.molina@pucv.cl>
2025-01-03 23:55:18 -0300

    Add new functions to optimization of hyperparams

--- attentional_cpmp/utils/functions.py
+++ attentional_cpmp/utils/functions.py
@@ -45,3 +45,68 @@ def split_data(data, key, percentage):
     remaining_labels = [labels[i] for i in range(len(labels)) if i not in indices]
 
     return (np.stack(sampled_states), np.stack(sampled_labels)), (np.stack(remaining_states), np.stack(remaining_labels))
+
+def get_hyperparams(dictionary):
+    '''
+    Función para obtener los hiperparámetros del modelo a partir de un diccionario,
+    esto es útil puesto que un diccionario puede tener más elementos que los hiperparámetros.
+
+    Args:
+        dictionary (dict): Diccionario con los hiperparámetros del modelo.
+
+    Returns:
+        dict: Diccionario con los hiperparámetros del modelo.
+    '''
+    new_dictionary = {}
+
+    new_dictionary['num_stacks'] = dictionary['num_stacks']
+    new_dictionary['num_heads'] = dictionary['num_heads']
+    new_dictionary['epsilon'] = dictionary['epsilon']
+    new_dictionary['key_dim'] = dictionary['key_dim']
+    new_dictionary['value_dim'] = dictionary['value_dim']
+    new_dictionary['dropout'] = dictionary['dropout']
+    new_dictionary['rate'] = dictionary['rate']
+    new_dictionary['activation_hide'] = dictionary['activation_hide']
+    new_dictionary['activation_feed'] = dictionary['activation_feed']
+
+    new_dictionary['n_dropout_hide'] = dictionary['n_dropout_hide']
+    new_dictionary['n_dropout_feed'] = dictionary['n_dropout_feed']
+    new_dictionary['list_neurons_feed'] = dictionary['list_neurons_feed']
+    new_dictionary['list_neurons_hide'] = dictionary['list_neurons_hide']
+
+    return new_dictionary
+
+def get_config_model(dictionary):
+    
+    '''
+    Función para obtener los hiperparámetros y la configuración del modelo a partir de un diccionario.
+
+    Args:
+        dictionary (dict): Diccionario con los hiperparámetros y la configuración del modelo.
+    
+    Returns:
+        dict: Diccionario con los hiperparámetros y la configuración del modelo.
+    '''
+
+    new_dictionary = {}
+    new_dictionary['H'] = dictionary['H']
+    new_dictionary['metrics'] = dictionary['metrics']
+    new_dictionary['optimizer'] = dictionary['optimizer']
+    new_dictionary['loss'] = dictionary['loss']
+
+    new_dictionary['num_stacks'] = dictionary['num_stacks']
+    new_dictionary['num_heads'] = dictionary['num_heads']
+    new_dictionary['epsilon'] = dictionary['epsilon']
+    new_dictionary['key_dim'] = dictionary['key_dim']
+    new_dictionary['value_dim'] = dictionary['value_dim']
+    new_dictionary['dropout'] = dictionary['dropout']
+    new_dictionary['rate'] = dictionary['rate']
+    new_dictionary['activation_hide'] = dictionary['activation_hide']
+    new_dictionary['activation_feed'] = dictionary['activation_feed']
+
+    new_dictionary['n_dropout_hide'] = dictionary['n_dropout_hide']
+    new_dictionary['n_dropout_feed'] = dictionary['n_dropout_feed']
+    new_dictionary['list_neurons_feed'] = dictionary['list_neurons_feed']
+    new_dictionary['list_neurons_hide'] = dictionary['list_neurons_hide']
+
+    return new_dictionary
