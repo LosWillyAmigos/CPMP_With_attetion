0f43bce0d05f1fa655707c55344d20b52b673b53
slinking196 <matias.bugueno.b@mail.pucv.cl>
2025-01-27 00:19:22 -0300

    update file to fit a new model with others parameters

--- test/test_model.py
+++ test/test_model.py
@@ -6,6 +6,7 @@ from cpmp_ml.utils.adapters import AttentionModel
 from cpmp_ml.validations import validate_model
 from dotenv import load_dotenv
 import tensorflow as tf
+import numpy as np
 import os
 
 load_dotenv()
@@ -27,12 +28,20 @@ client_local.close()
 tf.config.threading.set_intra_op_parallelism_threads(10)
 tf.config.threading.set_inter_op_parallelism_threads(10)
 
-model_Sx7 = create_model(num_heads= 5, num_stacks= 7, H= 8)
+model_Sx7 = create_model(
+    num_heads= 5,
+    num_stacks= 7, 
+    H= 7,
+    key_dim= 8,
+    activation_hide= 'linear',
+    list_neurons_feed= [32, 24, 16],
+    list_neurons_hide= [32, 24, 16],
+)
 
 for _ in range(2):
     for stack in data_Sx7:
-        model_Sx7.fit(data_Sx7[stack]["States"], data_Sx7[stack]["Labels"], batch_size= 32, epochs= 10)
+        model_Sx7.fit(np.array(data_Sx7[stack]["States"]), np.array(data_Sx7[stack]["Labels"]), batch_size= 32, epochs= 10)
 
-model_Sx7.save("./models/Sx7/model_Sx7.h5")
+model_Sx7.save("./models/attentional/Sx7/model_Sx7.h5")
 
 validate_model(model_Sx7, GreedyV2(), AttentionModel(), 10, 7, 50, 1000, max_steps= 100)
